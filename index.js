f5d7fa12a903548bba2059f9e3f7d86e24a89e1c
Adnan Sayed <adnan.sayed.official@gmail.com>
2021-05-03 01:13:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Your code here
+function mapToNegativize (array) {
+  return array.map(x => x * -1);
+}
+
+function mapToNoChange (array) {
+  return array.map(x => x );
+}
+ function mapToDouble(array) {
+   return array.map( x => x * 2 );
+ }
+ function mapToSquare(array) {
+   return array.map( x => x * x );
+ }
+
+function reduceToTotal(array, startingPoint=0) {
+  const reducer = function(accumulator, currentValue){
+     return accumulator + currentValue }
+
+     return array.reduce(reducer, startingPoint);
+}
+
+function reduceToAllTrue(array){
+
+    const reducer = function(accumulator, currentValue){
+        if(!!accumulator == true && !!currentValue == true){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    return array.reduce(reducer, true)
+}
+function reduceToAnyTrue(array){
+
+    const reducer = function(accumulator, currentValue){
+        if(!!accumulator == true){
+            return true;
+        } else {
+            return !!currentValue;
+        }
+    }
+    return array.reduce(reducer, false)
+}
